a8b19db27befdba77ea9dc53654c404cf25f6043
marybarkhi <marybarkhi1996@gmail.com>
2025-08-14 13:54:27 +0330

    refactor: cherry pick ui pages and config server port

--- app/src/main/java/com/barkhi/app/AppApplication.java
+++ app/src/main/java/com/barkhi/app/AppApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppApplication.class, args);
+        try {
+            SpringApplication.run(AppApplication.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
